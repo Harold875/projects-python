dc35e69b8269897534f9d41c1d689bc54fd3048c
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-03-09 13:55:08 -0400

    Codigo funcionando, en Casi todoas las pruebas.

--- 02-time-calculator/time-calculator.py
+++ 02-time-calculator/time-calculator.py
@@ -24,6 +24,7 @@ def add_time(start, duration, start_day= None):
             t_inicial['minutos'] = f"0{t_inicial['minutos']}"
         if t_inicial['hora'] > 11 and t_inicial['am-pm'] == "PM":
             t_inicial["dias"] += 1
+            t_inicial['am-pm'] = "AM"
             
     t_inicial['hora'] += t_duracion['hora']
     
@@ -73,13 +74,14 @@ def add_time(start, duration, start_day= None):
 #     }
 
 # tests
-
 print(add_time('3:30 PM', '2:12'))
 print(add_time('11:55 AM', '3:12'))
 print(add_time('2:59 AM', '24:00') )
 print(add_time('11:59 PM', '24:05'))
 print(add_time('8:16 PM', '466:02'))
 print(add_time('3:30 PM', '2:12', 'Monday'))
-(add_time('2:59 AM', '24:00', 'saturDay'))
+print(add_time('2:59 AM', '24:00', 'saturDay'))
+print(add_time('11:59 PM', '24:05', 'Wednesday'))
+print(add_time('8:16 PM', '466:02', 'tuesday'))
 
 # print(add_time('3:00 PM', '3:10', 'mondAy'))
