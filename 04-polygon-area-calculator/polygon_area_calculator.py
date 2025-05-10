d6fd3255dec53b8f6d06a0d885b7bc95308acfb8
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2025-05-10 16:19:11 -0400

    add Square class

--- 04-polygon-area-calculator/polygon_area_calculator.py
+++ 04-polygon-area-calculator/polygon_area_calculator.py
@@ -46,12 +46,46 @@ class Rectangle:
 
 
 
-class Square:
-    pass
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+        
+    def __str__(self):
+        return f"{self.__class__.__name__}(side={self.width})"
+    
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+        
+    def set_height(self, height):
+        return self.set_side(height)
+
+    def set_height(self, width):
+        return self.set_side(width)
+
+    
+# rect = Rectangle(10,5)
+# rect1 = Rectangle(10,5)
+# print(rect)
+# print(rect.get_picture())
+# print(rect.get_amount_inside(rect1))
 
 
-rect = Rectangle(10,5)
-rect1 = Rectangle(10,5)
+
+rect = Rectangle(10, 5)
 print(rect)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
 print(rect.get_picture())
-print(rect.get_amount_inside(rect1))
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
